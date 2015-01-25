496b33775b2d0b441648ccaa42ba905a6db5b945
Druggist <m.najdora@gmail.com>
2015-01-25 14:37:56 +0100

    GameJam Version

---
+++ GlobalGJ/Assets/40 i 4/MusicOnClick.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicOnClick : MonoBehaviour {
+	private bool _correct = true, _endSound = false;
+	private KeyCode[] _word = new KeyCode[] {
+		KeyCode.Alpha4,
+		KeyCode.Alpha0,
+		KeyCode.I,
+		KeyCode.Alpha4
+	};
+	
+	GameObject Particles;
+	GameObject[] Sounds;
+	private ushort _readMarker = 0;
+	
+	void Awake () {
+		Particles = GameObject.Find("Particles");
+		Sounds = new GameObject[]{
+			GameObject.Find("Sound_3"),
+			GameObject.Find("Sound_1"),
+			GameObject.Find("Sound_2"),
+			GameObject.Find("Sound_6"),
+			GameObject.Find("Sound_4"),
+			GameObject.Find("Sound_5"),
+		};
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (Input.anyKeyDown) {
+
+			if (Input.GetKeyDown(_word [_readMarker])) {
+				Sounds[_readMarker].audio.Play();
+				Debug.Log("Playing sound "+Sounds[_readMarker].name+" for key "+_readMarker);
+			} else {
+				//GameLogic.Instance().Message(Constants.STATE ["LOSE"]);
+				_correct = false;
+				Sounds[4].audio.Play();
+				Debug.Log("Playing sound 4, readMarker="+_readMarker.ToString());
+			}
+			_readMarker++;
+
+			Particles.particleSystem.Play();
+		}
+
+		if (_readMarker >= _word.Length) {
+			GameObject _sound = Sounds[( _correct )?3:5];
+			if (!_endSound) {
+				_sound.audio.Play();
+				_endSound = true;
+			}
+			if(!_sound.audio.isPlaying) {
+				GameLogic.Instance().Message(Constants.STATE[( _correct )?"WIN":"LOSE"]);
+			}
+		}
+	}
+}
---
+++ GlobalGJ/Assets/Scripts/wdwdn.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wdwdn : MonoBehaviour {
+
+	public void Load (string name) {
+		GameLogic.Instance().LoadLevel(name);
+	}
+}
