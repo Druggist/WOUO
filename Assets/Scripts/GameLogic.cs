5d227102ae382a426c4057ac54ddca314f53cd81
Druggist <m.najdora@gmail.com>
2016-01-25 13:55:04 +0100

    Project cleanup, changed unity version to 5.3 & error fixes
    Cleaned up project dir
    Changed unity version to 5.3
    Fixed win error & smaller bugs
    Added proper gui

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public static class Constants {	
+	public static Dictionary<string, ushort> STATE = new Dictionary<string, ushort>{
+		{ "WIN", 1 },
+		{ "LOSE", 2 },
+		{ "TIMEOUT", 3 }
+	};
+}
+
+public class GameLogic : MonoBehaviour {
+	public string[] levels;
+	private Dictionary<string, ushort> levelStates = new Dictionary<string, ushort>();
+	private static GameLogic _instance;
+	private bool _completed;
+
+	public static GameLogic Instance() {
+		if (_instance == null) {
+			GameLogic logic = FindObjectOfType(typeof(GameLogic)) as GameLogic;
+			_instance = logic;
+		}
+		return _instance;
+	}
+
+	void Awake () {
+		GameObject.DontDestroyOnLoad(this.gameObject);
+		if (levels.Length == 0) {
+			levels[0] = Application.loadedLevelName;
+		}
+		Instance().levels = this.levels;
+	}
+
+	public void Message(ushort state) {
+		string levelName = Application.loadedLevelName;
+		if ( _completed ) {
+            SceneManager.LoadScene("win");
+		}
+		levelStates [levelName] = state;
+		Debug.Log("Current level name: "+levelName+" State: "+levelStates[levelName]);
+		List<string> availableLevels = new List<string>();
+		foreach (string name in levels) {
+			ushort levelState = 0;
+			if (!levelStates.TryGetValue(name, out levelState) ||
+				levelState != Constants.STATE ["WIN"]) {
+				availableLevels.Add(name);
+			}
+		}
+		if (availableLevels.Count == 0) {
+			print("You won the game!");
+			_completed = true;
+			Application.Quit();
+			return;
+		}
+		int randomIndex = Random.Range(0, availableLevels.Count);
+		string levelNameToLoad = availableLevels [randomIndex];
+		print("Loading level: " + levelNameToLoad);
+        if(levelNameToLoad == "") SceneManager.LoadScene("win");
+        else SceneManager.LoadScene(levelNameToLoad);
+    }
+
+	public void LoadLevel(string name){
+        SceneManager.LoadScene(name);
+	}
+
+	public void Exit () {
+		Debug.Log("Exiting!");
+		Application.Quit();
+	}
+
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+	public bool allowForward = true, allowBack = true, allowRight = true, allowLeft = true;
+	public float speed = 1f;
+	public bool playAnimation = true;
+	public int jumpForce = 150;
+	public Camera playerCamera = null;
+	public bool inputEnabled = true;
+	private Transform playerTransform;
+	private bool _onGround = false, _jump = false;
+	private Vector3  _offset = Vector3.zero;
+	private Collider collider;
+
+	// Use this for initialization
+	void Awake () {
+		//Physics.gravity = Vector3.down * 0.1f;
+		if (playerCamera != null) _offset = playerCamera.transform.position - transform.position;
+		collider = gameObject.GetComponentInChildren<Collider>();
+	}
+
+	void Start () {
+
+	}
+
+	void Update () {
+		if (transform.position.y <= -20) {
+			GameLogic.Instance().Message(Constants.STATE["LOSE"]);
+		}
+
+		if (Input.GetKey(KeyCode.Space)) {
+			_jump = true;
+		}
+
+		_onGround = Physics.Raycast(transform.position, Vector3.down, collider.bounds.extents.y);
+
+		if (playerCamera != null) {
+			playerCamera.transform.position = transform.position + _offset;
+			playerCamera.transform.LookAt(transform.position);
+			//Debug.Log(cam.transform.position.ToString());
+		}
+	}
+
+	IEnumerator WaitForAnimation(Animation _anim)
+	{
+		do {
+			yield return null;
+		} while (_anim.isPlaying);
+	}
+
+
+	void OnTriggerEnter(Collider c) {
+		if (c.gameObject.tag == "Finish") {
+			if ( playAnimation == true ) {
+				Animator _anim = gameObject.GetComponentInChildren<Animator>();
+				inputEnabled = false;
+				transform.rotation = Quaternion.FromToRotation(Vector3.zero, Vector3.up);
+				_anim.Play("cube_Rotation");
+				GameLogic.Instance().Message(Constants.STATE["WIN"]);
+			} else {
+				GameLogic.Instance().Message(Constants.STATE["WIN"]);
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (inputEnabled == false) return;
+
+		float sideInput = Input.GetAxis("Horizontal");
+		float frontInput = Input.GetAxis("Vertical");
+		//Vector3 vectorTurn = Vector3.zero;
+
+		if (frontInput != 0) {
+			sideInput = 0;
+			//vectorTurn = frontInput * Vector3.left;
+		} else if (sideInput != 0) {
+			frontInput = 0;
+			//vectorTurn = sideInput * Vector3.back;
+		}
+
+		if ((!allowForward && frontInput > 0) ||
+			(!allowBack && frontInput < 0)) {
+			frontInput = 0;
+		}
+		if ((!allowRight && sideInput > 0) || 
+			(!allowLeft && sideInput < 0)) {
+			sideInput = 0;
+		}
+		
+		Vector3 moveForce = sideInput * Vector3.right * speed + frontInput * Vector3.forward * speed + Vector3.up * GetComponent<Rigidbody>().velocity.y;
+		
+		
+		GetComponent<Rigidbody>().velocity = moveForce;
+
+		//Debug.Log(_onGround && _jump);
+		if (_jump && _onGround) {
+			GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce);
+			_jump = false;
+			//Debug.Log(Vector3.up * jumpForce);
+		}
+		//Debug.Log(vectorTurn);
+	}
+}
